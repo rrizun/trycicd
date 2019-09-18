ad96d713ec3a68eb33759febcb519230ec7d500f
rrizun <rrizun>
2019-09-18 09:01:08 -0700

    initial checkin

---
+++ src/main/java/foo/Main.java
@@ -0,0 +1,14 @@
+package foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle
+ */
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+	}
+}
